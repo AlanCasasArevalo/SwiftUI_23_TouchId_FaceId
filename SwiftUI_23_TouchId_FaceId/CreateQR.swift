996db327098c500a18188a1606d371dc3a62f5a1
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-05 09:36:22 +0200

    [FEATURE][QR GENERATOR] Qr generator was implemented from a String

--- SwiftUI_23_TouchId_FaceId/CreateQR.swift
+++ SwiftUI_23_TouchId_FaceId/CreateQR.swift
@@ -1,9 +1,39 @@
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct CreateQR: View {
+    
+    @State private var textToCoding = ""
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text("Hello, CreateQR")
+        VStack (alignment: .center, spacing: 20) {
+            TextField("Texto a codificar", text: $textToCoding)
+                .padding(.bottom)
+            Image(uiImage: qrGenerator(textToCoding: textToCoding) ?? UIImage())
+                .interpolation(.high)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 250, height: 250, alignment: .center)
+        }
+        .padding(16)
+    }
+}
+
+extension CreateQR {
+    func qrGenerator (textToCoding: String) -> UIImage? {
+        let data = Data(textToCoding.utf8)
+        // La key en este caso es importante para que funcione que sea inputMessage
+        filter.setValue(data, forKey: "inputMessage")
+        
+        if let outputImage = filter.outputImage, let image = context.createCGImage(outputImage, from: outputImage.extent) {
+            return UIImage(cgImage: image)
+        }
+        
+        return nil
     }
 }
 
