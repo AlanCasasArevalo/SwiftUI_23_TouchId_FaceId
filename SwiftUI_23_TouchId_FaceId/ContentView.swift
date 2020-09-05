f3d7765c7765e69298f9d5505b9b6e2758610571
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-05 09:09:32 +0200

    Initial Commit

---
+++ SwiftUI_23_TouchId_FaceId/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_23_TouchId_FaceId
+//
+//  Created by Everis on 05/09/2020.
+//  Copyright © 2020 Alan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
