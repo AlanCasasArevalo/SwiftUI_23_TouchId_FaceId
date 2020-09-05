f74d8dd8db1647746246a64b3b30d983be001942
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-05 09:21:14 +0200

    [FEATURE][BIOMETRIC LOGIN] * Biometric login was implemented.

--- SwiftUI_23_TouchId_FaceId/ContentView.swift
+++ SwiftUI_23_TouchId_FaceId/ContentView.swift
@@ -1,11 +1,43 @@
 
 import SwiftUI
-
-
+import LocalAuthentication
 
 struct ContentView: View {
+    
+    @State private var unlock = false
+    
     var body: some View {
-        Text("Hello, World!")
+        return Group {
+            if unlock {
+                HomeView()
+            } else {
+                Text("Bloqueado no puedes pasar")
+            }
+        }
+    .onAppear(perform: authentication)
+    }
+}
+
+extension ContentView {
+    func authentication () {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Necesitamos autenticacion para poder ir a la home"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { (success, error) in
+                DispatchQueue.main.async {
+                    if success {
+                        self.unlock = true
+                    } else {
+                        print("No funciono la autenticacion ")
+                    }
+                }
+            }
+        } else {
+            print("No tiene ningun login biometrico")
+        }
+        
     }
 }
 
