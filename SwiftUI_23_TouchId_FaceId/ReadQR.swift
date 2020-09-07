82ce4dd81b99815f4cc99436807d507c8b8e0b14
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-07 10:52:16 +0200

    [FEATURE][QR SCANNER] * QR Scanner featured was implemented

--- SwiftUI_23_TouchId_FaceId/ReadQR.swift
+++ SwiftUI_23_TouchId_FaceId/ReadQR.swift
@@ -1,9 +1,32 @@
 
 import SwiftUI
+// imported library https://github.com/twostraws/CodeScanner
+import CodeScanner
 
 struct ReadQR: View {
+
+    @State private var showScanner = false
+    @State private var result = ""
+
     var body: some View {
-        Text("Hello, ReadQR")
+        VStack {
+            Button(action: {
+                self.showScanner = true
+            }) {
+                Text("Escanear QR")
+            }
+            .sheet(isPresented: $showScanner) {
+                // En el array de tipos de codigo podemos agregar los codigos que nos apetezca y esten aceptados
+                CodeScannerView(codeTypes: [.qr, .code128, .face]) { result in
+                    switch result {
+                    case .success(let code):
+                        self.result = code
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        }
     }
 }
 
